cdec234a60fdc7292f4d6343f20d84d70ff381da
HamzaDLM <hamzadellam@hotmail.com>
2022-05-29 13:20:44 +0100

    first commit

---
+++ hdlm-front/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Home msg="Nothing"/>
+</template>
+
+<script>
+import Home from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Oswald,sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #ffffff;
+  background: #141114;
+  min-height: 100vh;
+  max-height: 100%;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+}
+#app ::selection {
+  color: #fff;
+  background-color: #e329d5;
+}
+</style>
