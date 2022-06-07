9f0bd3cb5a32c939402c2da78ad01cf5825b7ddc
HamzaDLM <hamzadellam@hotmail.com>
2022-06-07 16:14:08 +0100

    tech stack & fix small nav

--- src/views/pages/techstackPage.vue
+++ src/views/pages/techstackPage.vue
@@ -23,14 +23,14 @@ export default {
         <!-- HEADER WHEN FULL SIZE -->
         <div class="d-none d-xl-block">
           <h1>
-            Hi, welcome to my <span>playground</span>
+            my ever expanding <span>techstack</span>
           </h1>
         </div>
         
         <!-- HEADER WHEN SMALL SIZE -->
         <div class="mb-4 d-xl-none d-m-block">
           <h2>
-            Hi, welcome to my <span>playground</span>
+            my ever expanding <span>techstack</span>
           </h2>
         </div>
 
@@ -40,31 +40,39 @@ export default {
           <div class="mb-5">
             <p class="writing-data">Backend Development</p>
             <!--<div class="position-relative">-->
-            <img class="card-img" src="@/assets/images/py.png" title='Python'>
-            <img class="card-img" src="@/assets/images/docker.png" title='Docker'>
-            <img class="card-img" src="@/assets/images/gitlab.png" title='Gitlab'>
-            <img class="card-img" src="@/assets/images/git.png" title='Git'>
-            <img class="card-img" src="@/assets/images/mongo.png" title='MongoDB'>
+            <img class="card-img" src="@/assets/images/technology/py.png" title='Python: General purpose programming language'>
+            <img class="card-img" src="@/assets/images/technology/django.png" title='Django: Backend Framework'>
+            <img class="card-img" src="@/assets/images/technology/fastapi.png" title='Python'>
+            <img class="card-img" src="@/assets/images/technology/flask.png" title='Python'>
+
+            <img class="card-img" src="@/assets/images/technology/mysql.png" title='MongoDB'>
+            <img class="card-img" src="@/assets/images/technology/mongo.png" title='MongoDB'>
+            <img class="card-img" src="@/assets/images/technology/postgres.png" title='MongoDB'>
+            <img class="card-img" src="@/assets/images/technology/redis.png" title='MongoDB'>
+            <img class="card-img" src="@/assets/images/technology/github.png" title='Gitlab'>
+            <img class="card-img" src="@/assets/images/technology/gitlab.png" title='Gitlab'>
+            <img class="card-img" src="@/assets/images/technology/docker.png" title='Docker'>
+            <img class="card-img" src="@/assets/images/technology/git.png" title='Git'>
+            <img class="card-img" src="@/assets/images/technology/uvicorn.png" title='Git'>
+            
             <!--</div>-->
           </div>
           <div class="mb-5">
             <p class="writing-data">Data Science</p>
             <!--<div class="position-relative">-->
-            <img class="card-img" src="@/assets/images/py.png" title='Python'>
-            <img class="card-img" src="@/assets/images/docker.png" title='Docker'>
-            <img class="card-img" src="@/assets/images/gitlab.png" title='Gitlab'>
-            <img class="card-img" src="@/assets/images/git.png" title='Git'>
-            <img class="card-img" src="@/assets/images/mongo.png" title='MongoDB'>
+            <img class="card-img" src="@/assets/images/technology/py.png" title='Python'>
+            <img class="card-img" src="@/assets/images/technology/powerbi.png" title='Docker'>
+            <img class="card-img" src="@/assets/images/technology/pandas.png" title='Docker'>
+            <img class="card-img" src="@/assets/images/technology/numpy.png" title='Docker'>
             <!--</div>-->
           </div>
           <div class="mb-5">
             <p class="writing-data">Frontend Development</p>
             <!--<div class="position-relative">-->
-            <img class="card-img" src="@/assets/images/py.png" title='Python'>
-            <img class="card-img" src="@/assets/images/docker.png" title='Docker'>
-            <img class="card-img" src="@/assets/images/gitlab.png" title='Gitlab'>
-            <img class="card-img" src="@/assets/images/git.png" title='Git'>
-            <img class="card-img" src="@/assets/images/mongo.png" title='MongoDB'>
+            <img class="card-img" src="@/assets/images/technology/js.png" title='Python'>
+            <img class="card-img" src="@/assets/images/technology/vue.png" title='Docker'>
+            <img class="card-img" src="@/assets/images/technology/html.png" title='Gitlab'>
+            <img class="card-img" src="@/assets/images/technology/css.png" title='Git'>
             <!--</div>-->
           </div>
 
